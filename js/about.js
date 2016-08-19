62581aa88b31c3cf3b40ee1d3575088db25bbeda
hsiao19 <yutunghsaio19@gmail.com>
2016-08-19 22:29:17 +0800

    Add about.js .

---
+++ js/about.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+  // skill bar
+  // http://codepen.io/kristenspencer/pen/alibf
+  $('.python').css('width', '75%');
+  $('.java').css('width', '25%');
+  $('.php').css('width', '15%');
+  $('.sql').css('width', '25%');
+  $('.javascript').css('width', '40%');
+  $('.nodejs').css('width', '10%');
+  $('.html_css').css('width', '60%');
+
+  $('.processing').css('width', '75%');
+  $('.arduino').css('width', '20%');
+  $('.unity').css('width', '40%');
+  $('.maya').css('width', '15%');
+  $('.autocad').css('width', '40%');
+  $('.sketchup').css('width', '50%');
+  $('.resolume').css('width', '30%');
+
+  $('.photoshop').css('width', '80%');
+  $('.illustrator').css('width', '80%');
+
+});
